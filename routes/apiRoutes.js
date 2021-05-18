941f5c8019a4d6cb2446a668623da6ebd0426e7d
Brittany Plummer <brittanyplummer@gmail.com>
2021-05-18 17:34:41 -0400

    updated apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,36 @@
 const db = require("../models");
 const router = require("express").Router();
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-router.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-    .sort({ day: -1 })
-    .limit(1)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+    db.Workout.aggregate([
+      { $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration"
+        }
+      }}
+    ]).sort( {
+      _id: -1
+    }).limit(10)
+      .then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.status(400).json(err);
+        });
+  });
+  router.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([
+      { $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration"
+        }
+      }}
+    ]).then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.status(400).json(err);
+        });
+  });
 router.post("/api/workouts", (req, res) => {
   db.Workout.create(req.body, (error, data) => {
     if (error) {
